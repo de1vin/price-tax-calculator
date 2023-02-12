90535b762c757210405b236d62d8a0901f70dc0f
vladimir_zinchenko <vladimir.zinchenko.tgn@gmail.com>
2023-02-12 18:11:39 +0300

    Realise price calculator

--- app/src/Controller/CalculatorController.php
+++ app/src/Controller/CalculatorController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 
 /**
